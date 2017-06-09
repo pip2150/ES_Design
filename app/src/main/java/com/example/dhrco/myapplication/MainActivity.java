e93fa03efb5fd6844b7329afcfee3d5a107af7e3
pip2150 <dhrcogns2002@hanmail.net>
2017-06-09 16:37:49 +0900

    opencv & tensorflow

---
+++ app/src/main/java/com/example/dhrco/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.dhrco.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private static String TAG= "MainActivity";
+
+    static{
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv successlly loaded");
+        }
+        else{
+            Log.d(TAG, "Opencv not loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}

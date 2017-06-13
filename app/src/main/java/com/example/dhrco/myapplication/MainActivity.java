99696406831546bdb37721cd07b605b9e9460185
pip2150 <dhrcogns2002@hanmail.net>
2017-06-13 22:18:42 +0900

    MarkerDetector

--- app/src/main/java/com/example/dhrco/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dhrco/myapplication/MainActivity.java
@@ -13,7 +13,14 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -102,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 
     @Override
     public void onCameraViewStarted(int width, int height) {
@@ -118,10 +125,47 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        MarkerDetector md = new MarkerDetector();
+
         mRgba = inputFrame.rgba();
 
+        /* Grayscale 변환 */
         Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
-        return imgGray;
+
+        /* 윤관석 도출 */
+        Imgproc.Canny(imgGray, imgCanny, 50, 100);
+
+        Vector<MatOfPoint> contours = new Vector<MatOfPoint>();
+        md.mfindContours(imgCanny, contours, MarkerDetector.MINContourPointsAllowed);
+
+        Vector<Marker> detectedMarkers = new Vector<Marker>();
+        md.findCandidates(contours, detectedMarkers);
+        Mat contoursfound = new Mat(imgCanny.size(), 0, new Scalar(255));
+        Imgproc.drawContours(contoursfound, contours, -1, new Scalar(1), 2);
+
+        Vector<MatOfPoint> detectedPoints = new Vector<MatOfPoint>();
+        for (int i = 0; i < detectedMarkers.size(); i++) {
+            MatOfPoint tmp = new MatOfPoint();
+            for (int j = 0; j < detectedMarkers.get(i).points.size().area(); j++) {
+                tmp.push_back(new MatOfPoint(new Point(detectedMarkers.get(i).points.toArray()[j].x, detectedMarkers.get(i).points.toArray()[j].y)));
+            }
+            detectedPoints.add(tmp);
+        }
+        for (int i = 0; i < detectedPoints.size(); i++) {
+            if (detectedPoints.get(i).size().area() == 4) {
+                MatOfPoint points = detectedPoints.get(i);
+                int x = Math.min((int)points.toArray()[2].x, (int)points.toArray()[3].x);
+                int y = Math.min((int)points.toArray()[3].y, (int)points.toArray()[0].y);
+                int width = Math.max((int)points.toArray()[0].x, (int)points.toArray()[1].x) - x;
+                int height = Math.max((int)points.toArray()[1].y, (int)points.toArray()[2].y) - y;
+                Rect area = new Rect(x, y, width, height);
+                Imgproc.rectangle(imgGray, new Point(x,y), new Point(x+width,y+height), new Scalar(255));
+//                Imgproc.rectangle(imgGray, area, new Scalar(255), 2);
+            }
+        }
+
+        return contoursfound;
     }
 }
+
 
